02bf3ddc85aa0f94b4e6f88113ea02c709a4e4da
bsh185 <b.m.shrah@gmail.com>
2015-02-16 13:55:44 +0200

    add more fixes to transaction to map more than one JSON object

--- routes/transactions.js
+++ routes/transactions.js
@@ -5,34 +5,85 @@ var express = require('express');
 var models  = require('..\\models');
 var db  = require('..\\models\\index.js');
 var router = express.Router();
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     res.render('index', { title: 'Company API' });
 });
 
 router.post('/', function (req,res) {
-    var data={
-        firstName:req.body.firstName,
-        lastName:req.body.lastName,
-        address:req.body.address,
-        phone:req.body.phone,
-        projectID:req.body.projectID,
-        deptID:req.body.deptID
-    };
-    db.sequelize.transaction(function (t) { // Note that we use a callback rather than a promise.then()
-          return models.employees.create(data, { transaction: t}).then(function (user) {
-          return user.update({deptID: 2}, { transaction: t}).then(function (user) {
-              return user.update({lastName: "Shrah"}, { transaction: t}).then(function (user) {
-                  return user.update({firstName:"bashar"}, { transaction: t})
-              });
-          });
-        });
-    }).then(function (result) {
-        res.json(result)
-    }).catch(function (err) {
-        res.statusCode=400;
-        res.json(err);
-    });
+    //console.log(req.body.employees[0])
+    if(req.body.employees){
+        var data= [];
+        for (index in req.body.employees){
+            data.push(req.body.employees[index]);
+        }
+        var re=[];
+        var count=0;
+        for(i in data) {
+            db.sequelize.transaction(function (t) { // Note that we use a callback rather than a promise.then()
+                return models.employees.create(data[i], {transaction: t}).then(function (user) {
+                    return user.update({deptID: 2}, {transaction: t}).then(function (user) {
+                        return user.update({lastName: "Shrah"}, {transaction: t}).then(function (user) {
+                            return user.update({firstName: "bashar"}, {transaction: t})
+                        });
+                    });
+                });
+            }).then(function (result) {
+                re.push(result);
+                count++;
+                if(count==data.length){
+                    res.json(re);
+                }
+            }).catch(function (err) {
+                if(count==0){
+                    res.json(err);
+                    count++;
+                }
+
+            });
+        }
+    }
+     if(req.body.Dept){
+        console.log("works");
+    }
+
 });
 
 
+
 module.exports = router;
+
+
+
+
+
+/* var data={
+ firstName:obj[0].firstName,
+ lastName:obj[0].lastName,
+ address:obj[0].address,
+ phone:obj[0].phone,
+ projectID:obj[0].projectID,
+ deptID:obj[0].deptID
+ };*/
+/* var rea=[];
+ var count=0;
+ data.forEach(function (item) {
+ db.sequelize.transaction(function (t) { // Note that we use a callback rather than a promise.then()
+ return models.employees.create(item, {transaction: t}).then(function (user) {
+ return user.update({deptID: 2}, {transaction: t}).then(function (user) {
+ return user.update({lastName: "Shrah"}, {transaction: t}).then(function (user) {
+ return user.update({firstName: "bashar"}, {transaction: t})
+ });
+ });
+ });
+ }).then(function (result) {
+ count++;
+ rea.push(result);
+ if(count==data.length){
+ res.json(rea);
+
+ }
+ }).catch(function (err) {
+ res.json(err);
+ return false;
+ });
+ });*/
